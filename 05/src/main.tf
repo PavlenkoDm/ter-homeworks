76af8c3c50a5ec7b15359c8d3cb997928368cbb2
PavlenkoDm <dmpavlenko07@gmail.com>
2025-10-04 13:20:48 +0300

    tflint & checkov issues fix

--- 05/src/main.tf
+++ 05/src/main.tf
@@ -1,84 +1,84 @@
-# resource "yandex_vpc_network" "develop" {
-#   name = var.vpc_name
-# }
-# resource "yandex_vpc_subnet" "develop" {
-#   name           = var.vpc_name
-#   zone           = var.default_zone
-#   network_id     = yandex_vpc_network.develop.id
-#   v4_cidr_blocks = var.default_cidr
-# }
-
-# module "vpc_dev" {
-#   source   = "./vpc"
-#   env_name = var.vpc_name
-#   zone     = var.default_zone
-#   cidr     = var.default_cidr[0]
-# }
-
-
-# data "template_file" "cloudinit" {
-#   template = file("./cloud-init.yml")
-  
-#   vars = {
-#     ssh_public_key = file("~/.ssh/id_ed25519.pub")
-#   }
-# }
-
-# module "marketing_vm" {
-#   source = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
-
-#   env_name = var.vpc_name
-
-#   network_id = module.vpc_dev.network_id
-#   subnet_zones = [var.default_zone]
-#   subnet_ids = [module.vpc_dev.subnet_id]
-
-#   instance_name = "marketing-web"
-#   instance_count = 1
-
-#   image_family = var.image_family
-
-#   public_ip = true
-  
-#   labels = { 
-#     owner   = "student",
-#     project = "marketing"
-#   }
-
-#   metadata = {
-#     user-data = data.template_file.cloudinit.rendered
-#     serial-port-enable = 1
-#   }
-# }
-
-# module "analytics_vm" {
-#   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
-
-#   env_name       = var.vpc_name
-
-#   network_id     = module.vpc_dev.network_id
-#   subnet_zones   = [var.default_zone]
-#   subnet_ids     = [module.vpc_dev.subnet_id]
-
-#   instance_name  = "analytics-web"
-#   instance_count = 1
-
-#   image_family   = var.image_family
-  
-#   public_ip      = true
-
-#   labels = { 
-#     owner   = "student",
-#     project = "analytics"
-#   }
-
-#   metadata = {
-#     user-data          = data.template_file.cloudinit.rendered
-#     serial-port-enable = 1
-#   }
-# }
-
-resource "random_password" "any_uniq_name" {
-  length = 20
+resource "yandex_vpc_network" "develop" {
+  name = var.vpc_name
 }
+resource "yandex_vpc_subnet" "develop" {
+  name           = var.vpc_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
+}
+
+module "vpc_dev" {
+  source   = "./vpc"
+  env_name = var.vpc_name
+  zone     = var.default_zone
+  cidr     = var.default_cidr[0]
+}
+
+
+data "template_file" "cloudinit" {
+  template = file("./cloud-init.yml")
+  
+  vars = {
+    ssh_public_key = file("~/.ssh/id_ed25519.pub")
+  }
+}
+
+module "marketing_vm" {
+  source = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=7020e1fc51e53485c9fc06bffff199970cb68234"
+
+  env_name = var.vpc_name
+
+  network_id = module.vpc_dev.network_id
+  subnet_zones = [var.default_zone]
+  subnet_ids = [module.vpc_dev.subnet_id]
+
+  instance_name = "marketing-web"
+  instance_count = 1
+
+  image_family = var.image_family
+
+  public_ip = true
+  
+  labels = { 
+    owner   = "student",
+    project = "marketing"
+  }
+
+  metadata = {
+    user-data = data.template_file.cloudinit.rendered
+    serial-port-enable = 1
+  }
+}
+
+module "analytics_vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=7020e1fc51e53485c9fc06bffff199970cb68234"
+
+  env_name       = var.vpc_name
+
+  network_id     = module.vpc_dev.network_id
+  subnet_zones   = [var.default_zone]
+  subnet_ids     = [module.vpc_dev.subnet_id]
+
+  instance_name  = "analytics-web"
+  instance_count = 1
+
+  image_family   = var.image_family
+  
+  public_ip      = true
+
+  labels = { 
+    owner   = "student",
+    project = "analytics"
+  }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered
+    serial-port-enable = 1
+  }
+}
+
+# resource "random_password" "any_uniq_name" {
+#   length = 20
+# }
 
